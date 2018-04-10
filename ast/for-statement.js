f127cb41e210208c7646d3cc8765b4a82b2e3ca9
Jackson Myers <jackson@Jacksons-MacBook-Pro.local>
2018-04-09 17:57:15 -0700

    lots and lots of fixes

---
+++ ast/for-statement.js
@@ -0,0 +1,22 @@
+module.exports = class WhileStatement {
+  constructor(decl, test, assign, body) {
+    Object.assign(this, { decl, test, assign, body });
+  }
+
+  analyze(context) {
+    this.decl.analyze(context);
+    this.test.analyze(context);
+    this.assign.analyze(context);
+    const bodyContext = context.createChildContextForLoop();
+    this.body.forEach(s => s.analyze(bodyContext));
+  }
+
+  optimize() {
+    this.decl = this.decl.optimize();
+    this.test = this.test.optimize();
+    this.assign = this.assign.optimize();
+    this.body.map(s => s.optimize()).filter(s => s !== null);
+    // Suggested: Look for returns/breaks in the middle of the body
+    return this;
+  }
+};
