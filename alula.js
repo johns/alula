d89cc081693cd9e099f4f8e5ec5e046b080b9fa3
Sam Chami <sc01914@gmail.com>
2018-05-03 22:48:17 -0700

    Make alula compatible with a (non-working) generator

--- alula.js
+++ alula.js
@@ -1,5 +1,29 @@
 #!/usr/bin/env node
 
+/*
+ * An alula Compiler
+ *
+ * This is a command line application that compiles an alula program from
+ * a file. There are three options:
+ *
+ * ./alula.js -a <filename>
+ *     writes out the AST and stops
+ *
+ * ./alula.js -i <filename>
+ *     writes the decorated AST then stops
+ *
+ * ./alula.js <filename>
+ *     compiles the alula program to JavaScript, writing the generated
+ *     JavaScript code to standard output.
+ *
+ * ./alula.js -o <filename>
+ *     optimizes the intermediate code before generating target JavaScript.
+ *
+ * Output of the AST and decorated AST uses the object inspection functionality
+ * built into Node.js.
+ */
+
+
 const { argv } = require('yargs')
   .usage('$0 [-a] [-o] [-i] filename')
   .boolean(['a', 'o', 'i'])
@@ -11,6 +35,7 @@ const { argv } = require('yargs')
 const fs = require('fs');
 const util = require('util');
 const parse = require('./syntax/parser');
+// require('./backend/javascript-generator');
 
 /* eslint-disable no-console */
 fs.readFile(argv._[0], 'utf-8', (err, text) => {
@@ -18,14 +43,20 @@ fs.readFile(argv._[0], 'utf-8', (err, text) => {
     console.error(err);
     return;
   }
-  const program = parse(text);
+
+  let program = parse(text);
   if (argv.a) {
     console.log(util.inspect(program, { depth: null }));
     return;
   }
   program.analyze();
+  if (argv.o) {
+    program = program.optimize();
+  }
   if (argv.i) {
     console.log(util.inspect(program, { depth: null }));
+    return;
   }
+  program.gen();
 });
 /* eslint-enable no-console */
